07831a78a1800f8820a4120fd49289a6755af95b
Matt Waraich <mattsinghwaraich@gmail.com>
2019-10-20 23:27:38 -0700

    Items page completed - further styling needed

--- client/src/pages/Home/HomeContainer.js
+++ client/src/pages/Home/HomeContainer.js
@@ -1,20 +1,20 @@
-// import React, { Component } from "react";
-// import Home from "./Home";
+import React, { Component } from "react";
+import Home from "./Home";
 
-// class HomeContainer extends Component {
-//   render() {
-//     return (
-//       <Query query={GET_ITEMS} variables={{ id: 1 }}>
-//         {({ loading, error, data }) => {
-//           if (loading) return "Loading";
-//           if (error) return `Error: ${error}`;
-//           if (data) {
-//             console.log(data);
-//             return <Items items={data.items} />;
-//           }
-//         }}
-//       </Query>
-//     );
-//   }
-// }
-// export default HomeContainer;
+class HomeContainer extends Component {
+  render() {
+    return (
+      <Query query={GET_ITEMS} variables={{ id: 1 }}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading";
+          if (error) return `Error: ${error}`;
+          if (data) {
+            console.log(data);
+            return <Items items={data.items} />;
+          }
+        }}
+      </Query>
+    );
+  }
+}
+export default HomeContainer;
